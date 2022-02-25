5d35f6a13b62b3e3bf4d440a6142fe726ebdc8b8
Isaac Wassouf <izakwassouf@gmail.com>
2022-02-26 00:39:05 +0100

    hello from Isaac first commit :)

--- src/index.ts
+++ src/index.ts
@@ -14,43 +14,43 @@ bot.start((ctx) => {
     ctx.reply(`Hello ${userName} !`);
 });
 
-bot.on('document', async (ctx) => {
-    const { file_id: fileId } = ctx.update.message.document;
-    const fileUrl = await ctx.telegram.getFileLink(fileId);
-    const response = await axios.get(fileUrl.toString());
-    ctx.reply('I read the file for you! The contents were:\n\n' + response.data);
-});
+// bot.on('document', async (ctx) => {
+//     const { file_id: fileId } = ctx.update.message.document;
+//     const fileUrl = await ctx.telegram.getFileLink(fileId);
+//     const response = await axios.get(fileUrl.toString());
+//     ctx.reply('I read the file for you! The contents were:\n\n' + response.data);
+// });
 
 
 
-bot.help((ctx) => {
-    ctx.reply('Send /start to receive a greeting');
-    ctx.reply('Send /keyboard to receive a message with a keyboard');
-    ctx.reply('Send /quit to stop the bot');
-});
+// bot.help((ctx) => {
+//     ctx.reply('Send /start to receive a greeting');
+//     ctx.reply('Send /keyboard to receive a message with a keyboard');
+//     ctx.reply('Send /quit to stop the bot');
+// });
 
-bot.command('quit', (ctx) => {
-    // Explicit usage
-    ctx.telegram.leaveChat(ctx.message.chat.id);
-    // Context shortcut
-    ctx.leaveChat();
-});
-bot.command('keyboard', (ctx) => {
-    ctx.reply(
-        'Keyboard',
-        Markup.inlineKeyboard([
-            Markup.button.callback('First option', 'first'),
-            Markup.button.callback('Second option', 'second'),
-        ])
-    );
-});
-bot.on('text', (ctx) => {
-    ctx.reply(
-        'You choose the ' +
-        (ctx.message.text === 'first' ? 'First' : 'Second') +
-        ' Option!'
-    );
-});
+// bot.command('quit', (ctx) => {
+//     // Explicit usage
+//     ctx.telegram.leaveChat(ctx.message.chat.id);
+//     // Context shortcut
+//     ctx.leaveChat();
+// });
+// bot.command('keyboard', (ctx) => {
+//     ctx.reply(
+//         'Keyboard',
+//         Markup.inlineKeyboard([
+//             Markup.button.callback('First option', 'first'),
+//             Markup.button.callback('Second option', 'second'),
+//         ])
+//     );
+// });
+// bot.on('text', (ctx) => {
+//     ctx.reply(
+//         'You choose the ' +
+//         (ctx.message.text === 'first' ? 'First' : 'Second') +
+//         ' Option!'
+//     );
+// });
 
 bot.launch();
 // Enable graceful stop
