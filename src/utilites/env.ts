7010845d5d73063cc14c638c458550cdb412d18d
Milad <m_jawhara@prokoders.com>
2022-03-01 01:39:35 +0200

    -Update env

--- src/utilites/env.ts
+++ src/utilites/env.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export default (key: string) => {
-    return process.env[key]
+export default (key: string, val: string = ''): string => {
+    if (!process.env[key]) return val;
+    return process.env[key] as string;
 }
