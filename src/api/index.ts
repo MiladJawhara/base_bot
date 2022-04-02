7c70dfaadb9cb43786f102293c01bc4c296de00e
Milad Jawhara <milad.jawhara@gmail.com>
2022-04-03 00:20:17 +0300

    - Come on

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import { User } from './../db/models/User';
 
 const app = express();
 
 app.use(cors());
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const result: {
+        users: any[]
+    } = {
+        users: []
+    };
+    (await User.find()).forEach(user => {
+        result.users.push({
+            name: user.f_name
+        });
+    });
+    res.json(result);
 });
 
 
